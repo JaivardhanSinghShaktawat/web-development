4cb11af1009b6942fbdcbe6991b80926b6ae8e9f
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-09-11 18:41:41 +0530

    Day 0 - Orientation Class

---
+++ 00-orientation/01-orientation-class.js
@@ -0,0 +1,17 @@
+/* 
+--------------------------------- Orientation Class ---------------------------------
+
+--> Consistency and dedication is the key to success. 
+    Create twitter and linkedin account and share your journey. 
+    DSA is used behind the scenes in development.(MongoDB internals , Redis etc)
+    AI can create websites but it cannot create scalable , maintainable websites. 
+
+    Best time to start something is now. 
+
+    Don't use too much animations for your website , it will make your website
+    slow.Create a website that should workd everywhere and it should load in a 
+    slow network as well. 
+
+    UI/UX : ui stands for user interface and ux stands for user experience.
+    Too much animation creates a bad user experience.
+*/
