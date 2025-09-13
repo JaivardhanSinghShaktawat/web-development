cca3b407ad94614060cfd35c0b5163a667e7600e
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-09-13 20:17:09 +0530

    Day 1 - Modern Routers And Protocols

---
+++ 01-fundamentals/11-protocols.js
@@ -0,0 +1,39 @@
+/* 
+----------------------------- Protocols And Fundamentals --------------------------
+
+--> What are protocols ? 
+    A protocol is a set of rules.
+
+--> What is HTTP/HTTPS ? 
+    HTTP/HTTPS are communication protocol that is used for communication between 
+    client and the server.
+    It determines/specifies how the data is shared/transfer between client and 
+    the server.
+
+    HTTP : HyperText Transfer Protocol
+    HTTPS : HyperText Transfer Protocol Secure (HTTP + TLS/SSL Certificate)
+
+    HTTPS : It is more secure.Here the data is first encryted and than shared/transfer 
+    between client and the server. 
+
+    Note : During production always use HTTPS encrypted server.
+    It encrypts the data before sharing so that no one can read the data between 
+    client and the server.
+
+    FTP and SSH protocols are used to share the files
+    SMTP protocols are used in mails.
+    HTTP/HTTPS are used in web.
+
+    Anyone can read the data between client and server if you are using http 
+    protocol.
+
+    In HTTPS , data is encrypted and than shared between client and the server. 
+    It is more secure. 
+    Here data is first encrypted before sending and than send.The data is then
+    decrypted in the receiver side.  
+
+--> What is SSH protocol ? 
+    SSH stands for secure shell protocol.It's a file transfer protocol that is used for
+    transfering/sharing the files over an unsecured network.
+
+*/
