e686cafe8184b4db5dd01204c0efaaceedc7ff27
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-09-13 20:20:00 +0530

    Day 1 - Web Development

---
+++ 01-fundamentals/12-web-development.js
@@ -0,0 +1,58 @@
+/* 
+---------------------------------- Web Development ---------------------------------
+
+--> What is Web Development ? 
+    The development of websites/web application is called as web development. 
+
+--> Client : any machine/person that requests something from the server. 
+    Server : a program that sends something to the client as a response. 
+
+    Client request to the server and server generally sends HTML , CSS and JS 
+    file to the client.Client/browser will render the HTML , CSS and JS into a 
+    website. 
+
+--> Note : Open any website and go to inspect.With the help of inspect or 
+    element tab we can change the content of the DOM. 
+
+    Note : Many traders use this inspect method to change their portfolio.
+
+--> HTML : it is used to structure the content of a web page. 
+    CSS : it is used to make website beautiful. 
+    JavaScript : it is used to make website dynamic and interactive. 
+
+    Note : HTML , CSS are not programming langauges. 
+
+--> What is MERN Stack ? 
+    It stands for MongoDB , Express , React And NodeJs. 
+
+    Frontend : we can create it using HTML , CSS and JavaScript or ReactJs. 
+    Backend : server + database 
+    Frontend is the UI. 
+
+    Database : it is used to store the data. 
+    server : it serves the files based on the request. 
+    Backend contains the business logic.All the important code. 
+
+    Backend contains business logic
+    Authentication 
+    Authorization 
+    api 
+    database schema etc. 
+
+    Frontend request to the server and server request to the database for data. 
+
+    NodeJs + ExpressJs is used to create backend in JavaScript.
+    Spring + SpringBoot is used to create backend in Java. 
+
+    Note : Database is not good for storing images and videos.We store videos and
+    imags on cloud. 
+
+--> Frontend Developer : Developers who write code that will execute on the client 
+    side. 
+
+    Backend Developer : Developers who write code that will execute on the server
+    side. 
+
+    FullStack Developer : Developers who write code that execute both on client
+    as well as server side.
+*/
