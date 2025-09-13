64ae4638787594a41e8f7febd04bec46f71d0b95
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-09-13 20:05:23 +0530

    Day 1 - Network And Internet

---
+++ 01-fundamentals/01-network-and-internet.js
@@ -0,0 +1,20 @@
+/* 
+------------------------------ Network And Internet --------------------------------
+
+--> What is Network ? 
+    Computers connected together via any medium is called as network.
+
+--> What is Internet ? 
+    It is a global network of network where multiple devices like laptop , 
+    computer etc are connected to each other via optic fibre cables/physical devices
+    or via 3G or 4G across the entire world and through routers and switches in 
+    the entire country or cities.
+    The combination of different local area networks together is called as 
+    internet. 
+
+    Computer uses TCP/IP protocol to communicate with each other. 
+
+    Note : Go to google and search for internet map and their you will find out the 
+    internet cables map.
+
+*/
