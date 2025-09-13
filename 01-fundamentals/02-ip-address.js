f816aa4f20ee570b43b27f06f171bbc6c52c8c78
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-09-13 20:07:42 +0530

    Day 1 - IP Address

---
+++ 01-fundamentals/02-ip-address.js
@@ -0,0 +1,53 @@
+/* 
+------------------------------------- IP Address -----------------------------------
+
+--> What is IP address ? 
+    IP address stands for internet protocol. 
+    It is the temporary address of the computer on the internet given by ISP.
+    The IP address is the address of the machine/computer on the internet.
+    The IP address helps to identify the computer on the internet.
+    It is the address of the machine that is connected to the internet.
+
+    To send data from A to B , we need the IP address , Mac address and Port number
+    of A as well as B. 
+
+    Structure of a message 
+
+    SIP | SMAC | SPORT | Data/Request | RIP | RMAC | RPORT
+
+    SIP : sender IP address
+    SMAC : sender MAC address
+    SPORT : sender Port number
+    Data/Request : Data you want to send or any request. 
+    RIP : receiver IP address. 
+    RMAC : receiver MAC address. 
+    RPORT : receiver PORT number. 
+
+    IP address is simply the address of the computer on the internet. 
+
+    Note : When we connect to the internet , than a temporary IP address is given to 
+    our device and when we disconnect from the internet than that IP address is taken 
+    from us.This happens all the time. 
+    IP address is not permanent address.It is the temporary address. 
+    Once you disconnect from the internet than your IP address can also allocate
+    to a different device. 
+
+    Suppose x IP address is given to A when A is connected to the internet.When A 
+    disconnects from the internet than x IP address is taken from it.The x can be
+    given to another device B as well.Same IP address cannot be given to multiple
+    devices at the same time.
+
+    How to see the IP address of your device ? 
+    Go to whatismyipaddress.com website to see your IP address. 
+
+--> Who gives IP address to our device ? 
+    ISP (Interet Service Provider) gives a temporary IP address to our device each
+    time when we connect to the internet.
+    ISP are those that provides internet services like jio , airtel etc. 
+
+    Note : When we on the internet than ISP provides a temporary IP address for 
+    our device and when we disconnect from the internet than ISP takes back that
+    IP address from us.IP address is very dynamic.It is not permanent address and it
+    is not the same.It changes everytime when we on/connect to the internet.
+
+*/
