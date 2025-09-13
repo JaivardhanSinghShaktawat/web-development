1ed15379d148c6717c8a177d7e9da1e9cbbe8276
JaivardhanSinghShaktawat <jaivardhansinghshaktawat5@gmail.com>
2025-09-13 20:14:14 +0530

    Day 1 - LAN Switch And Router

---
+++ 01-fundamentals/08-lan-switch-router.js
@@ -0,0 +1,55 @@
+/* 
+-------------------------------- LAN Switch And Router -----------------------------
+
+--> Local Area Network And Switch 
+
+    Switch : it is a central device through which all the computers are connected.
+    All the computers connects with the switch vai a wire/ethernet. 
+
+    If A computer wants to communicate/send data to B computer than A will send
+    the data to switch.Switch will than send the data to the B computer. 
+    Similarly , if B wants to communicate with A or some other computer than B 
+    will send the data to the switch and switch will send the data to the A or
+    some other computer. 
+
+    Advantage of switch 
+
+    We can connect more computers easily.It is more scalable. 
+    It saves wires as we don't have to connect all the computers to each other. 
+
+    Disadvantage of switch 
+
+    single point of failure : it means if switch fails than no one can communicate
+    with each other. 
+
+    LAN : it s a local area network where multiple devices are connected to each
+    other via a single entity which is switch.All the computers are connected
+    to a single device which is switch. 
+
+--> Router : it is used to connect different LAN networks together. 
+    Router is used to route the data. 
+    It's main task is to connect different local area networks together. 
+    Each LAN should have a router.Both the routers are connected together via
+    a wire as well as both the routers are connected to their LAN switch as well. 
+
+
+    c1 --> switch  --- router1 ----- router2 ---- switch --> c2
+
+    routers are connected to switch as well as another router. 
+    Internet : combining all the local networs/local area networks via routers
+    is called as internet. 
+
+--> Go to submarinecablesmap website to see the different cables that creates 
+    internet. 
+
+    The optic fibre cables connects the different country together.
+    In each country , there are multiple cables that connects local area network. 
+    The local area networks are connected together via routers. 
+    The local computers are connected together via switch.
+
+--> How data is send from mobile ? 
+    We send and receive data from mobile to the tower.The tower has cables which is 
+    present underground and the data travel through that underground cables. 
+    This is how internet works. 
+
+*/
